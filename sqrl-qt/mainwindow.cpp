608c90a0e6e61e6e2f43605f4ef886d16edafa39
Scott Weldon <scott@scott-weldon.com>
2014-11-26 23:40:59 -0800

    Formatting

--- sqrl-qt/mainwindow.cpp
+++ sqrl-qt/mainwindow.cpp
@@ -2,13 +2,10 @@
 #include "ui_mainwindow.h"
 
 MainWindow::MainWindow(QWidget *parent) :
-    QMainWindow(parent),
-    ui(new Ui::MainWindow)
-{
-    ui->setupUi(this);
+  QMainWindow(parent), ui(new Ui::MainWindow) {
+  ui->setupUi(this);
 }
 
-MainWindow::~MainWindow()
-{
-    delete ui;
+MainWindow::~MainWindow() {
+  delete ui;
 }
