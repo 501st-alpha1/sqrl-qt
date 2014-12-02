51a10ad6d42a25f8e03f5c1a7a5d092a5d3d42e2
Scott Weldon <scott@scott-weldon.com>
2014-12-01 18:47:17 -0800

    Added auto-loading of default ID

--- sqrl-qt/mainwindow.cpp
+++ sqrl-qt/mainwindow.cpp
@@ -1,10 +1,29 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 #include "identitycreationwindowpage1.cpp"
+#include <QString>
+#include <QFile>
+#include <QDir>
 
 MainWindow::MainWindow(QWidget *parent) :
   QMainWindow(parent), ui(new Ui::MainWindow) {
   ui->setupUi(this);
+
+  QString filename = QDir::homePath() + "/.sqrl/ident.txt";
+  QFile file(filename);
+  QString key;
+
+  if (file.open(QIODevice::ReadOnly)) {
+      QTextStream stream(&file);
+      key = stream.readLine();
+  }
+
+  if (key.length() == 64) {
+    ui->label_2->setText(key);
+  }
+  else {
+    ui->label_2->setText("No key found, or key is invalid");
+  }
 }
 
 MainWindow::~MainWindow() {
