c96eb55fbed1ced45847dac1a9de753fcf657467
Scott Weldon <scott@scott-weldon.com>
2014-12-09 13:58:41 -0800

    Fixed key references again
    Now using domain-specific key. Verification is broken again though.

--- sqrl-qt/sqrlidentity.cpp
+++ sqrl-qt/sqrlidentity.cpp
@@ -92,6 +92,7 @@ QByteArray SqrlIdentity::makeDomainPrivateKey(QString domain) {
   QCA::Initializer init;
 
   QByteArray arg = domain.toLocal8Bit();
+  QByteArray key((char*)this->getKey());
 
   if (!QCA::isSupported("hmac(sha256)")) {
     qDebug() << "hmac(sha256) is not supported!";
@@ -100,9 +101,9 @@ QByteArray SqrlIdentity::makeDomainPrivateKey(QString domain) {
   else {
     QCA::MessageAuthenticationCode hmacObject("hmac(sha256)",QCA::SecureArray());
 
-    QCA::SymmetricKey keyObject(this->key);
+    QCA::SymmetricKey keyObject(key);
 
-    hmacObject.setup(this->key);
+    hmacObject.setup(key);
 
     QCA::SecureArray converter(arg);
 
@@ -114,15 +115,19 @@ QByteArray SqrlIdentity::makeDomainPrivateKey(QString domain) {
   }
 }
 
-QByteArray SqrlIdentity::signMessage(QString message, QByteArray privateKey) {
+QByteArray SqrlIdentity::signMessage(QString message, QByteArray key) {
   if (sodium_init() == -1) {
     qDebug() << "Error: sodium_init failed.";
     return NULL;
   }
 
+  // Prepare the private key
+  unsigned char privateKey[crypto_sign_SECRETKEYBYTES];
+  memcpy(privateKey, key, crypto_sign_SECRETKEYBYTES);
+
   // Now the public key
   unsigned char publicKey[crypto_sign_PUBLICKEYBYTES];
-  crypto_sign_ed25519_sk_to_pk(publicKey, this->key);
+  crypto_sign_ed25519_sk_to_pk(publicKey, privateKey);
 
   /*
    * Debugging
@@ -135,7 +140,7 @@ QByteArray SqrlIdentity::signMessage(QString message, QByteArray privateKey) {
 
   printf("private key ");
   for (unsigned int i = 0; i < crypto_sign_SECRETKEYBYTES; ++i) {
-    printf("%02x", (unsigned char)this->key[i]);
+    printf("%02x", (unsigned char)privateKey[i]);
   }
   printf("\n");
   /*
@@ -145,7 +150,7 @@ QByteArray SqrlIdentity::signMessage(QString message, QByteArray privateKey) {
   unsigned char* actualMessage = (unsigned char*)message.toAscii().constData();
   unsigned char sig[crypto_sign_BYTES];
 
-  crypto_sign_detached(sig, NULL, actualMessage, message.length(), this->key);
+  crypto_sign_detached(sig, NULL, actualMessage, message.length(), privateKey);
 
   if (crypto_sign_verify_detached(sig, actualMessage, message.length(),
                                   publicKey) != 0)
