073645e658438de2f425e67921a6d59edbf6094c
Scott Weldon <scottw@scvdata.com>
2015-01-28 12:35:07 -0800

    Show what URL is being POSTed to

--- sqrl-qt/sqrlidentity.cpp
+++ sqrl-qt/sqrlidentity.cpp
@@ -329,6 +329,7 @@ bool SqrlIdentity::authenticate(QUrl url) {
   QNetworkAccessManager* manager = new QNetworkAccessManager();
   QUrl post("https://" + message);
   QNetworkRequest request(post);
+  qDebug() << "post:" << post;
 
   request.setHeader(QNetworkRequest::ContentTypeHeader,
                     "application/x-www-form-urlencoded");
