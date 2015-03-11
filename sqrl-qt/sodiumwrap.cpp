0a0fd8b5e79e51e657dc818a7e0ea953e91983af
Scott Weldon <scottw@scvdata.com>
2015-03-11 12:22:57 -0700

    Refactor getUnsignedCharFromString()

--- sqrl-qt/sodiumwrap.cpp
+++ sqrl-qt/sodiumwrap.cpp
@@ -2,7 +2,8 @@
 #include "sodiumwrap.h"
 #include <QDebug>
 
-static unsigned char* getUnsignedCharFromString(QString str, int len) {
+static unsigned char* getUnsignedCharFromString(QString str) {
+  int len = str.length();
   unsigned char* result = new unsigned char[crypto_sign_SEEDBYTES];
   for (int i = 0; i < len; ++i) {
     result[i] = (unsigned char)str.at(i).toAscii();
@@ -11,12 +12,12 @@ static unsigned char* getUnsignedCharFromString(QString str, int len) {
 }
 
 unsigned char* SodiumWrap::getKeyFromQString(QString input) {
-  return getUnsignedCharFromString(input, input.length());
+  return getUnsignedCharFromString(input);
 }
 
 unsigned char* SodiumWrap::hmacSha256(unsigned char* key, QString message) {
   unsigned char* out = new unsigned char[crypto_auth_hmacsha256_BYTES];
-  unsigned char* in = getUnsignedCharFromString(message, message.length());
+  unsigned char* in = getUnsignedCharFromString(message);
 
   crypto_auth_hmacsha256(out, in, message.length(), key);
 
@@ -31,8 +32,7 @@ unsigned char* SodiumWrap::hmacSha256(unsigned char* key, QString message) {
 unsigned char* SodiumWrap::signDetached(QString message,
                                         unsigned char* privateKey,
                                         unsigned char* publicKey) {
-  unsigned char* actualMessage = getUnsignedCharFromString(message,
-                                                           message.length());
+  unsigned char* actualMessage = getUnsignedCharFromString(message);
   unsigned char* out = new unsigned char[SodiumWrap::SIG_LEN];
 
   crypto_sign_detached(out, NULL, actualMessage, message.length(), privateKey);
