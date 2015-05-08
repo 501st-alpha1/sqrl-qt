37950f00128ddeb0f137c92d6f4655913f0c624e
Scott Weldon <scott@scott-weldon.com>
2015-05-07 22:40:51 -0700

    Add auth confirmation window

---
+++ sqrl-qt/authenticationconfirmation.cpp
@@ -0,0 +1,14 @@
+#include "authenticationconfirmation.h"
+#include "ui_authenticationconfirmation.h"
+
+AuthenticationConfirmation::AuthenticationConfirmation(QWidget *parent) :
+    QWidget(parent),
+    ui(new Ui::AuthenticationConfirmation)
+{
+    ui->setupUi(this);
+}
+
+AuthenticationConfirmation::~AuthenticationConfirmation()
+{
+    delete ui;
+}
---
+++ sqrl-qt/authenticationconfirmation.h
@@ -0,0 +1,22 @@
+#ifndef AUTHENTICATIONCONFIRMATION_H
+#define AUTHENTICATIONCONFIRMATION_H
+
+#include <QWidget>
+
+namespace Ui {
+class AuthenticationConfirmation;
+}
+
+class AuthenticationConfirmation : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit AuthenticationConfirmation(QWidget *parent = 0);
+    ~AuthenticationConfirmation();
+
+private:
+    Ui::AuthenticationConfirmation *ui;
+};
+
+#endif // AUTHENTICATIONCONFIRMATION_H
