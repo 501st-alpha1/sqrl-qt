daca4cbfe11b551e786fa2b69e5218c9b5922d8f
Scott Weldon <scott@scott-weldon.com>
2015-05-09 09:51:09 -0700

    Add some debugging code

--- sqrl-qt/authenticationconfirmation.cpp
+++ sqrl-qt/authenticationconfirmation.cpp
@@ -1,5 +1,6 @@
 #include "authenticationconfirmation.h"
 #include "ui_authenticationconfirmation.h"
+#include <QDebug>
 
 AuthenticationConfirmation::AuthenticationConfirmation(QWidget *parent) :
   QWidget(parent),
@@ -17,13 +18,16 @@ AuthenticationConfirmation::~AuthenticationConfirmation() {
 }
 
 void AuthenticationConfirmation::on_pushButton_clicked() {
+  qDebug() << "Pressed No";
   return;
 }
 
 void AuthenticationConfirmation::on_pushButton_3_clicked() {
+  qDebug() << "Pressed Options";
   return;
 }
 
 void AuthenticationConfirmation::on_pushButton_2_clicked() {
+  qDebug() << "Pressed Yes";
   return;
 }
