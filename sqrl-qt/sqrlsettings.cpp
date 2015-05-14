7602ae6f304fe507f82e5f6779bd54e7d351ddc9
Scott Weldon <scottw@scvdata.com>
2015-05-14 10:23:11 -0700

    Add options flags to settings class

--- sqrl-qt/sqrlsettings.cpp
+++ sqrl-qt/sqrlsettings.cpp
@@ -1,4 +1,14 @@
 #include "sqrlsettings.h"
 
+// Options Flags
+const int OPT_CHECK_FOR_UPDATES = 0x0001;
+const int OPT_ALWAYS_ASK_FOR_IDENTITY = 0x0002;
+const int OPT_SQRL_ONLY = 0x0004;
+const int OPT_HARD_LOCK = 0x0008;
+const int OPT_WARN_MITM = 0x0010;
+const int OPT_WIPE_HINT_SUSPEND = 0x0020;
+const int OPT_WIPE_HINT_USER_CHANGE = 0x0040;
+const int OPT_WIPE_HINT_IDLE = 0x0080;
+
 SqrlSettings::SqrlSettings() {
 }
