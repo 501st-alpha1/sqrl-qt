58b4493439e888dca5cebf8bb65d8b9922419aa6
Scott Weldon <scottw@scvdata.com>
2015-06-29 12:59:51 -0700

    Add length of inner block and FIXME

--- sqrl-qt/sqrlsettings.cpp
+++ sqrl-qt/sqrlsettings.cpp
@@ -8,10 +8,12 @@ bool SqrlSettings::getOption(const int optCode) {
 }
 
 QByteArray SqrlSettings::toQByteArray() {
+  // FIXME: ensure that bytes are aligned correctly (pad with 0s if needed).
   QByteArray ret;
   ret.append("sqrldata");
   ret.append(157); // length of block
   ret.append(1); // type of block
+  ret.append(45); // length of inner block
 
   qDebug() << ret;
   qDebug() << ret.size();
