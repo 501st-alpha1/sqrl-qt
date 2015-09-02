74faa8ed9e93c02ec1b0f5c01d9a0d17a38ce925
Scott Weldon <scott@scott-weldon.com>
2015-09-01 22:14:12 -0700

    Add more byte counts

--- sqrl-qt/sqrlsettings.cpp
+++ sqrl-qt/sqrlsettings.cpp
@@ -12,9 +12,9 @@ QByteArray SqrlSettings::toQByteArray() {
   // FIXME: ensure that bytes are aligned correctly (pad with 0s if needed).
   QByteArray ret;
   ret.append("sqrldata");
-  ret.append(157); // length of block
-  ret.append(1); // type of block
-  ret.append(45); // length of inner block
+  ret.append(157); // length of block (2 bytes)
+  ret.append(1); // type of block (2 bytes)
+  ret.append(45); // length of inner block (2 bytes)
   // TODO 12 bytes aes-gcm iv init vector
   // TODO 16 bytes scrypt random salt
   // TODO 1 byte scrypt log n factor
